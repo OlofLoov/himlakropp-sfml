f399bd9ca10bcf2a4b39acd148204893a1624137
Olof Lööv <olofloov@users-MBP.home>
2022-09-05 14:59:18 +0200

    Rm cmaketest code

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,22 +2,6 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 
-class MyClass
-{
-    public:
-    int i = 0;
-    MyClass()
-    {
-        std::cout << "Created my class" << std::endl;
-    }
-
-    MyClass(const MyClass &myClass)
-    {
-        i = myClass.i;
-        std::cout << "Copy constructor called" << std::endl;
-    }
-};
-
 int main()
 {
     Game game;
